9bb1ec15ae043d94c605d76241d88fceb50a865e
liurc <liurc@jpush.cn>
2018-07-12 10:47:20 +0800

    flask init

--- pic_receiver.py
+++ pic_receiver.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*-
+
+from flask import request, Flask
+import time
+import os
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def get_frame():
+    start_time = time.time()
+    upload_file = request.files['file']
+    old_file_name = upload_file.filename
+    if upload_file:
+        file_path = os.path.join('/home/ubuntu/upload/', old_file_name)
+        upload_file.save(file_path)
+        print "success"
+        print('file saved to %s' % file_path)
+        duration = time.time() - start_time
+        print('duration:[%.0fms]' % (duration*1000))
+        return 'success'
+    else:
+        return 'failed'
+
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port=32216)
